d3f6a0d25d10602a894908fa7a4440fbfb426b7d
Njine <brikab@gmail.com>
2024-07-01 15:12:05 +0000

    customers model

--- finanzy/finanzy/settings.py
+++ finanzy/finanzy/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "vehicles",
+    "customers",
+    "loans",
+    "reports",
+    "payments",
 ]
 
 MIDDLEWARE = [
