3e9f733e3aeb2aba78947b9f2e5af864f84d8f65
Njine <brikab@gmail.com>
2024-07-01 15:35:05 +0000

    payments models

--- finanzy/payments/models.py
+++ finanzy/payments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+import math
 
 # Create your models here.
+class Payment(models.Model):
+    loan = models.ForeignKey(Loan, on_delete=models.CASCADE)
+    payment_date = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Payment of {self.amount} on {self.payment_date}"
