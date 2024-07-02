3e3f85ac0b0c8e6ee361d23a6f1e412513609f9c
Njine <brikab@gmail.com>
2024-07-02 01:10:41 +0000

    db and Docker

--- finanzy/finanzy/settings.py
+++ finanzy/finanzy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,10 +80,9 @@ WSGI_APPLICATION = "finanzy.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default='postgres://postgres:password@db:5432/postgres'
+    )
 }
 
 
