9895845e412ee34fc6f97ffdde78c04dbdc05fdd
Njine <brikab@gmail.com>
2024-07-04 22:18:09 +0000

    added the customers and vehicles views

--- finanzy/customers/views.py
+++ finanzy/customers/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Customer, Vehicle, Loan
-from .forms import LoanForm
+from .models import Customer
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
--- finanzy/vehicles/views.py
+++ finanzy/vehicles/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Customer, Vehicle, Loan
-from .forms import LoanForm
-from django.contrib.auth.decorators import login_required
+from .models import Vehicle
 
 # Create your views here.
+def vehicle_list(request):
+    vehicles = Vehicle.objects.filter(available=True)
+    return render(request, 'vehicle_list.html', {'vehicles': vehicles})
+
+def vehicle_detail(request, vehicle_id):
+    vehicle = get_object_or_404(Vehicle, id=vehicle_id)
+    return render(request, 'vehicle_detail.html', {'vehicle': vehicle})
