aa6301fa1543e0e3d2e5c7e025ed45d5b63bd78d
Njine <brikab@gmail.com>
2024-07-04 18:10:30 +0000

    tax pin to customer model

--- finanzy/customers/models.py
+++ finanzy/customers/models.py
@@ -7,6 +7,7 @@ class Customer(models.Model):
     phone_number = models.CharField(max_length=15)
     address = models.CharField(max_length=255)
     date_of_birth = models.DateField()
+    tax_pin = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.user.username
