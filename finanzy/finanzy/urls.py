2d86b6a01ba1fd9830e4a6052ca0b3a03ca636dd
Njine <brikab@gmail.com>
2024-07-05 19:05:37 +0000

    urls and views

--- finanzy/finanzy/urls.py
+++ finanzy/finanzy/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('customers/', include('customers.urls')),
+    path('vehicles/', include('vehicles.urls')),
+    path('loans/', include('loans.urls')),
+    path('payments/', include('payments.urls')),
+    #path('reports/', include('reports.urls')),
 ]
