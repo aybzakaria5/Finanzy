75d1b357dbb98ef767a62f9918c29bc51c040752
Ayoub Zakarya <85744427+aybzakaria5@users.noreply.github.com>
2024-07-09 08:43:14 +0000

    updating the requirments to include redis and celery for docker iamge

--- finanzy/vehicles/urls.py
+++ finanzy/vehicles/urls.py
@@ -11,7 +11,7 @@ from rest_framework.routers import DefaultRouter
 from .views import VehicleViewSet
 
 router = DefaultRouter()
-router.register(r'veichles', VehicleViewSet)
+router.register(r'vehicles', VehicleViewSet)
 
 urlpatterns = [
     path('api', include(router.urls)),
