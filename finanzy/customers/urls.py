72bfff66be8c3b9739755de192d0bbdcba9078cd
Njine <brikab@gmail.com>
2024-07-10 01:03:11 +0000

    Added customer views and urls

--- finanzy/customers/urls.py
+++ finanzy/customers/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomerViewSet
+from .views import CustomerViewSet, customer_detail
 
 router = DefaultRouter()
 router.register(r'customers', CustomerViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('customer/<int:customer_id>/', customer_detail, name='customer_detail'),
 ]
