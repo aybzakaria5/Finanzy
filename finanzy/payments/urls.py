2ee1521047b4b0238b76e3aa9b3ea25ba408a7f3
Njine <brikab@gmail.com>
2024-07-10 01:26:40 +0000

    edited payment views and urls

--- finanzy/payments/urls.py
+++ finanzy/payments/urls.py
@@ -1,20 +1,13 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('payments/', views.payment_list, name='payment_list'),
-#     path('payment/<int:payment_id>/', views.payment_detail, name='payment_detail'),
-#     path('loan/<int:loan_id>/payment/', views.payment_create, name='payment_create'),
-# ]
-
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PaymentViewSet
+from .views import PaymentViewSet, payment_list, payment_detail, payment_create
 
 router = DefaultRouter()
 router.register(r'payments', PaymentViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('payment/list/', payment_list, name='payment_list'),
+    path('payment/<int:payment_id>/', payment_detail, name='payment_detail'),
+    path('payment/create/<int:loan_id>/', payment_create, name='payment_create'),
 ]
