b18e16e45fdd3abaf18344597fc2e55f2c079c9e
Njine <brikab@gmail.com>
2024-07-10 01:16:00 +0000

    added loan viewsets and urls

--- finanzy/loans/urls.py
+++ finanzy/loans/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LoanViewSet
+from .views import LoanViewSet, loan_application, loan_detail, loan_list
 
 router = DefaultRouter()
-router.register(r'loan', LoanViewSet)
+router.register(r'loans', LoanViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('loan/application/', loan_application, name='loan_application'),
+    path('loan/<int:loan_id>/', loan_detail, name='loan_detail'),
+    path('loan/list/', loan_list, name='loan_list'),
 ]
-
--- finanzy/loans/views.py
+++ finanzy/loans/views.py
@@ -1,36 +1,35 @@
-# from django.shortcuts import render, get_object_or_404, redirect
-# from django.http import HttpResponse
-# from .models import Customer, Vehicle, Loan
-# from .forms import LoanForm
-# from django.contrib.auth.decorators import login_required
-
-# # Create your views here.
-# @login_required
-# def loan_application(request):
-#     if request.method == 'POST':
-#         form = LoanForm(request.POST)
-#         if form.is_valid():
-#             loan = form.save(commit=False)
-#             loan.customer = request.user.customer
-#             loan.save()
-#             return redirect('loan_detail', loan_id=loan.id)
-#     else:
-#         form = LoanForm()
-#     return render(request, 'loan_application.html', {'form': form})
-
-# @login_required
-# def loan_detail(request, loan_id):
-#     loan = get_object_or_404(Loan, id=loan_id)
-#     return render(request, 'loan_detail.html', {'loan': loan})
-
-# @login_required
-# def loan_list(request):
-#     loans = Loan.objects.filter(customer=request.user.customer)
-#     return render(request, 'loan_list.html', {'loans': loans})
-
-from rest_framework import viewsets
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 from .models import Loan
+from .forms import LoanForm
+from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets
 from .serializers import LoanSerializer
+
+# Create your views here.
+@login_required
+def loan_application(request):
+    if request.method == 'POST':
+        form = LoanForm(request.POST)
+        if form.is_valid():
+            loan = form.save(commit=False)
+            loan.customer = request.user.customer
+            loan.save()
+            return redirect('loan_detail', loan_id=loan.id)
+    else:
+        form = LoanForm()
+    return render(request, 'loan_application.html', {'form': form})
+
+@login_required
+def loan_detail(request, loan_id):
+    loan = get_object_or_404(Loan, id=loan_id)
+    return render(request, 'loan_detail.html', {'loan': loan})
+
+@login_required
+def loan_list(request):
+    loans = Loan.objects.filter(customer=request.user.customer)
+    return render(request, 'loan_list.html', {'loans': loans})
+
 
 class LoanViewSet(viewsets.ModelViewSet):
     queryset = Loan.objects.all()
